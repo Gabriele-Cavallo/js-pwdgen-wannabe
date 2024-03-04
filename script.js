0b57b2823f145040cca90bcc17a8d5971f9646a6
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-04 14:00:19 +0100

    feat: add print screen user message

--- script.js
+++ script.js
@@ -3,5 +3,11 @@
 // 1-Chiedere all'utente il suo nome
 const userName = prompt('Ciao, dimmi il tuo nome!');
 // 2-Chiedere all'utente il suo cognome
+const userSurname = prompt('Ciao, dimmi il tuo cognome!');
 // 3-Chiedere all'utente il suo colore preferito
+const userColor = prompt('Ciao, dimmi il tuo colore preferito!');
 // 4-Generare la password in base ai dati inseriti
+const userPswd = `${userName}${userSurname}${userColor}23`;
+// 5-Stampa a schermo la password
+const userMessage = `${'Ecco la tua password personalizzata!! Password: '}${userPswd}`;
+document.getElementById('pswdGen').innerHTML = userMessage;
