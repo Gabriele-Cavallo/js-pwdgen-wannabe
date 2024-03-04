25c887b95a4f67926024daf56fc19b79875ef419
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-04 13:31:22 +0100

    feat: add JS structure

---
+++ script.js
@@ -0,0 +1,6 @@
+// Genera una password dopo aver chiesto all'utente nome, cognome, colore preferito e termina con 23
+
+// 1-Chiedere all'utente il suo nome
+// 2-Chiedere all'utente il suo cognome
+// 3-Chiedere all'utente il suo colore preferito
+// 4-Generare la password in base ai dati inseriti
