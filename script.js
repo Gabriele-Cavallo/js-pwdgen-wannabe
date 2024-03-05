6e815d85954a7a56fddf6755c770b7d1db2fbced
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-05 10:20:05 +0100

    feat: add finalNumber variable

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ const userColor = prompt('Ciao, dimmi il tuo colore preferito!');
 // 4-Chiedere all'utente un numero
 const userNumber = prompt('Ciao, dimmi un numero!');
 let userNumberAsValue = parseInt (userNumber);
+const finalNumber = userNumberAsValue + 23;
 // 5-Generare la password in base ai dati inseriti
-const userPswd = `${userName}${userSurname}${userColor}${userNumberAsValue + 23}`;
+const userPswd = `${userName}${userSurname}${userColor}${finalNumber}`;
 // 6-Stampa a schermo la password
 const userMessage = `${'Ecco la tua password personalizzata!! Password: '}${userPswd}`;
 document.getElementById('pswdGen').innerHTML = userMessage;
